0d61e82e26bfef3291418eb61b1702010cf44129
Vinod-M-1 <vinod552006630@gmail.com>
2026-03-07 00:47:12 +0530

    Made path changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.get('/home', (req,res)=>{
+app.get('/', (req,res)=>{
     res.render('home.ejs');
 })
 
